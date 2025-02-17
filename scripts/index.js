0966e730eda8f79003af62fd53c4c9c83f55627e
Irina <vlsova.irina.ru@yandex.ru>
2025-02-17 19:15:34 +0300

    create, delete functions

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const places = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template') .content;
+
+function createCard(data, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const card = cardElement.querySelector('.places__item');
+  const img = cardElement.querySelector('.card__image');
+  const button = cardElement.querySelector('.card__delete-button');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  img.src = data.link;
+  cardTitle.textContent = data.name;
+
+  button.addEventListener('click', () => deleteCard(card));
+
+  return card;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach(data => {
+  const card = createCard(data, deleteCard);
+  places.append(card);
+});
+
