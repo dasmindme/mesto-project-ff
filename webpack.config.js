4d71bf5028a931cc788a67bc5460da7ea5e94c35
Irina Vlsova <vlsova.irina.ru@yandex.ru>
2025-03-19 00:43:06 +0300

    10 commit

--- webpack.config.js
+++ webpack.config.js
@@ -34,11 +34,18 @@ module.exports = {
                 exclude: '/node_modules/'
             },
             {
-                test: /\.(png|svg|jpe?g|gif|woff(2)?|eot|ttf|otf)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext]'
-                }
+              test: /\.(png|svg|jpg|jpeg|gif)$/,
+              type: 'asset/resource',
+              generator: {
+                  filename: 'images/[name].[hash][ext]',
+              }
+          },
+          {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: 'asset/resource',
+              generator: {
+              filename: 'fonts/[name].[hash][ext]',
+            }
             },
             {
                 test: /\.node$/,
